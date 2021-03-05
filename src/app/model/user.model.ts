efda22c54b945997f536e0a0d25b2f802ef9c6bc
amanndeepak <amann2593@gmail.com>
2021-03-05 21:16:51 +0530

    final project commit

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+
+  constructor(
+    public displayName: string, 
+    public email: string,
+    public emailVerified: string,
+    public photoURL: string,
+    public isAnonymous: boolean,
+    public uid: string,
+    public providerData: string) {}
+}
+
